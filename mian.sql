5058fbd5f2616ba69eb4b6df88f01006965eed2b
will2012 <will.song.ws@gamil.com>
2015-06-02 00:02:33 +0800

    Update mian.sql

--- mian.sql
+++ mian.sql
@@ -21,7 +21,8 @@ CREATE TABLE `mian_post` (
  insert into mian_category(category_name) values('java');
  insert into mian_category(category_name) values('ios');
  insert into mian_category(category_name) values('android');
-
+ insert into mian_category(category_name) values('c++');
+  insert into mian_category(category_name) values('软件测试');
   commit;
   insert into mian_post(category_id, post_title, post_content) values(%s, %s, %s);
   select post_title,post_content from mian_post where category_id=? limit 0,20
